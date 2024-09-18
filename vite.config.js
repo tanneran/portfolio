bf0b2c4e2fe422fae095012f15451c23b09e2fcf
alinaaaaan <anhuiyi216@gmail.com>
2024-09-18 17:17:11 -0400

    change resume

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
-  build: {
-    outDir:'../dist'
-  }
+  // build: {
+  //   outDir:'../dist'
+  // }
 })
