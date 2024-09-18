b817513e08752f62e2ba388b79aedd8326750a60
alinaaaaan <anhuiyi216@gmail.com>
2024-09-18 15:45:14 -0400

    third

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: '/portfolio/',
+  build: {
+    outDir:'../dist'
+  }
 })
